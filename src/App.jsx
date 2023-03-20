def089285b4a4f1db5715abcd96c1919f2d5b911
Adam Rose <adrotog@gmail.com>
2023-03-20 14:57:32 -0400

    .

--- src/App.jsx
+++ src/App.jsx
@@ -15,21 +15,21 @@ function App() {
   );
 }
 
+function ErrorFallback({ error, resetErrorBoundary }) {
+  useEffect(() => {
+    resetErrorBoundary();
+  }, ["CONSTANT"]);
+
+  return (
+    <div>
+      <p>Something went wrong:</p>
+      <pre>{error.toString()}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  );
+}
+
 function StandardErrorBoundary({ children }) {
-  function ErrorFallback({ error, resetErrorBoundary }) {
-    useEffect(() => {
-      resetErrorBoundary();
-    }, ["CONSTANT"]);
-
-    return (
-      <div>
-        <p>Something went wrong:</p>
-        <pre>{error.toString()}</pre>
-        <button onClick={resetErrorBoundary}>Try again</button>
-      </div>
-    );
-  }
-
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>{children}</ErrorBoundary>
   );
